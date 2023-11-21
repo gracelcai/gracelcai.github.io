c492672dc0474bba048973b741a8df8d37dac6d4
Grace Cai <grace.cai7@gmail.com>
2023-11-21 17:30:52 -0500

    added navbar and menu

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -13,7 +13,6 @@ import { default as Navbar } from "./components/Navbar/Navbar.jsx";
 const App = () => {
 	return (
 		<div className="app">
-			<h1>App</h1>
 			<Navbar />
 			<Header />
 			<About />
