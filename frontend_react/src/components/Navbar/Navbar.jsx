8c263d24a143c3ab3d679cc5d2ddcd08c6ddebb9
Grace Cai <grace.cai7@gmail.com>
2023-11-23 10:59:59 -0500

    navbar highlight

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,49 +1,68 @@
-import React from 'react'
-import './Navbar.scss'
-import {images} from '../../constants/index'
-import {HiMenuAlt4, HiX} from 'react-icons/hi'
-import {motion} from 'framer-motion'
-import { useState } from 'react'
+import React from "react";
+import "./Navbar.scss";
+import { images } from "../../constants/index";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
-  return (
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
-        <img src={images.logo} alt='logo' width={200}/>
-      </div>
-      <ul className='app__navbar-links'>
-        {['home', 'about', 'skills', 'experience', 'projects', 'contact'].map((item) => (
-        <li className='app__flex p-text'key={`<link-${item}`}>
-          <div>
-            <a href={`#${item}`}>{item}</a>
-          </div>
-        </li>
-        ))}
-      </ul>
+	const [toggle, setToggle] = useState(false);
+	const [page, setPage] = useState("home");
 
-      <div className='app__navbar-menu'>
-          <HiMenuAlt4 onClick={() => setToggle(true)}/>
+	const handlePage = (page) => {
+		setPage(page);
+	};
+	return (
+		<nav className="app__navbar">
+			<div className="app__navbar-logo">
+				<img src={images.logo} alt="logo" width={200} />
+			</div>
+			<ul className="app__navbar-links">
+				{["home", "about", "skills", "experience", "projects", "contact"].map(
+					(item) => (
+						<li
+							className={`app__navbar-links-item app__flex p-text ${
+								page === item ? "item-active" : ""
+							}`}
+							key={`<link-${item}`}
+							onClick={() => handlePage(item)}
+						>
+							<a href={`#${item}`}>{item}</a>
+						</li>
+					)
+				)}
+			</ul>
 
-          {toggle && (
-            <motion.div
-              whileInView={{x: [300,0]}}
-              transition={{duration: 0.85, ease: 'easeOut'}}
-            >
-              <HiX onClick={() => setToggle(false)} />
-              <ul>
-                {['home', 'about', 'skills', 'experience', 'projects', 'contact'].map((item) => (
-                <li key={item}>
-                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                </li>
-                ))}
-              </ul>
-              
-            </motion.div>
-          )}
-      </div>
-    </nav>
-  )
-}
+			<div className="app__navbar-menu">
+				<HiMenuAlt4 onClick={() => setToggle(true)} />
 
-export default Navbar
+				{toggle && (
+					<motion.div
+						whileInView={{ x: [300, 0] }}
+						transition={{ duration: 0.85, ease: "easeOut" }}
+					>
+						<HiX onClick={() => setToggle(false)} />
+						<ul>
+							{[
+								"home",
+								"about",
+								"skills",
+								"experience",
+								"projects",
+								"contact",
+							].map((item) => (
+								<li key={item}>
+									<a href={`#${item}`} onClick={() => setToggle(false)}>
+										{item}
+									</a>
+								</li>
+							))}
+						</ul>
+					</motion.div>
+				)}
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
